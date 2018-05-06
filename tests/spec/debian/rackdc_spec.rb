1bdb67b3d2b59bf44fc080562f1331dc25db723e
Ben Dalling <b.dalling@locp.co.uk>
2018-05-06 22:04:14 +0100

    Allow a user to specify the DC and Rack.  Fixes #6.

---
+++ tests/spec/debian/rackdc_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../spec_helper'
+
+describe 'Debian Rack/DC Configuration File' do
+  describe file('/etc/cassandra/cassandra-rackdc.properties') do
+    it { should be_file }
+    it { should contain 'dc=DC1' }
+    it { should contain 'rack=RACK1' }
+  end
+end
