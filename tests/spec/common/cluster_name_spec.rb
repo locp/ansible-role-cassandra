b2bd3fa35b0f3c3773bfe03b9d4f6faf7afca120
Ben Dalling <b.dalling@locp.co.uk>
2018-05-12 08:34:32 +0100

    #9 Rubocop appeasement.

--- tests/spec/common/cluster_name_spec.rb
+++ tests/spec/common/cluster_name_spec.rb
@@ -3,9 +3,9 @@ require_relative '../spec_helper'
 describe 'Common Check Cluster Name' do
   @script = 'SELECT cluster_name FROM local;'
   @creds = '-u cassandra -p cassandra'
-  @cqlsh = "cqlsh $( hostname -i ) -k system"
+  @cqlsh = 'cqlsh $( hostname -i ) -k system'
 
   describe command("#{@cqlsh} #{@creds} -e '#{@script}'") do
-    its(:stdout) { should match /MyCassandraCluster/ }
+    its(:stdout) { should match(/MyCassandraCluster/) }
   end
 end
