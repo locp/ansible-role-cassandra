fb7918e0d5087fa844916882e646adef78ab0b48
Ben Dalling <b.dalling@locp.co.uk>
2018-12-13 22:11:09 +0000

    Find the IP address.

--- test/integration/default/inspec/cluster_name_spec.rb
+++ test/integration/default/inspec/cluster_name_spec.rb
@@ -1,6 +1,7 @@
 @script = 'SELECT cluster_name FROM local;'
 @creds = '-u cassandra -p cassandra'
-@cqlsh = 'cqlsh $( hostname -i ) -k system'
+@ip = command('hostname -I').stdout.strip
+@cqlsh = "cqlsh #{@ip} -k system"
 
 describe command("#{@cqlsh} #{@creds} -e '#{@script}'") do
   its(:stdout) { should match(/MyCassandraCluster/) }
