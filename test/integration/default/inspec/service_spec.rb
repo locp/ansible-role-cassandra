ef06134e51b628fc5db2f15e37e98cb1f4d69d81
Ben Dalling <b.dalling@locp.co.uk>
2018-12-13 21:39:24 +0000

    Ensure the service is correctly recognised.

--- test/integration/default/inspec/service_spec.rb
+++ test/integration/default/inspec/service_spec.rb
@@ -1,5 +1,13 @@
-describe service('cassandra') do
-  it { should be_enabled }
-  it { should be_running }
-  it { should be_installed }
+if os.family == 'redhat'
+  describe service('cassandra') do
+    it { should be_enabled }
+    it { should be_running }
+    it { should be_installed }
+  end
+else
+  describe upstart_service('cassandra') do
+    it { should be_enabled }
+    it { should be_running }
+    it { should be_installed }
+  end
 end
