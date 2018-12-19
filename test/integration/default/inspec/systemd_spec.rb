497082f96201af7f8b6ded788f997fe4457ff0eb
Ben Dalling <b.dalling@locp.co.uk>
2018-12-19 23:45:34 +0000

    Change systemd file location and minor changes to unit file (#35).

--- test/integration/default/inspec/systemd_spec.rb
+++ test/integration/default/inspec/systemd_spec.rb
@@ -1,9 +1,9 @@
 if os.name == 'fedora'
-  describe file('/usr/lib/systemd/system/cassandra.service') do
+  describe file('/etc/systemd/system/cassandra.service') do
     it { should exist }
   end
 else
-  describe file('/usr/lib/systemd/system/cassandra.service') do
+  describe file('/etc/systemd/system/cassandra.service') do
     it { should_not exist }
   end
 end
