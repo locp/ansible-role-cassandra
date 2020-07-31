a916e9c069df7b9f31797f744926c4c18ea40533
Ben Dalling <b.dalling@locp.co.uk>
2020-07-31 08:38:33 +0100

    Correct environment settings.

--- env/debian.sh
+++ env/debian.sh
@@ -1,3 +1,4 @@
+unset DOCKER_COMMAND
 export DOCKER_IMAGE="debian"
 export DOCKER_TAG_1="9"
 export DOCKER_TAG_2="10"
--- env/ubuntu.sh
+++ env/ubuntu.sh
@@ -1,3 +1,4 @@
+unset DOCKER_COMMAND
 export DOCKER_IMAGE="ubuntu"
 export DOCKER_TAG_1="18.04"
 export DOCKER_TAG_2="20.04"
